d65f762e8a62d4401fc4b6cd7e4c9fb283b8d71a
Alexander Morugin <nobilis@bk.ru>
2025-07-24 14:18:25 +0300

    fix AppHeading

--- components/page/AppHeading.vue
+++ components/page/AppHeading.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="heading">
-    <!-- <LayoutMainContainer> -->
-    <!-- <div class="heading__container"> -->
     <h1 class="heading__title">{{ title }}</h1>
 
     <ul class="heading__list">
@@ -10,11 +8,14 @@
         :key="item.id"
         class="heading__listItem"
       >
+        <img
+          src="/icons/triangle-filled-svgrepo-com.svg"
+          alt="Стрелка"
+          class="heading__listItemArrow"
+        />
         <span class="heading__listItemText">{{ item.text }}</span>
       </li>
     </ul>
-    <!-- </div> -->
-    <!-- </LayoutMainContainer> -->
   </div>
 </template>
 
@@ -25,25 +26,14 @@ const { title, headingMainList } = defineProps(['title', 'headingMainList']);
 <style scoped>
 .heading {
   position: relative;
-
   width: 100%;
   max-width: 1440px;
   margin: 0 auto;
-
   padding-top: 90px;
   padding-bottom: 20px;
-  /* padding-left: 20px;
-  padding-right: 20px; */
   z-index: 20;
-
   border: 1px solid red;
 }
-/* .heading__container {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 550px;
-} */
 .heading__title {
   width: 100%;
   max-width: 580px;
@@ -57,13 +47,20 @@ const { title, headingMainList } = defineProps(['title', 'headingMainList']);
   margin-top: 26px;
   padding-top: 10px;
   padding-bottom: 10px;
+  padding-left: 20px;
 }
 .heading__listItem {
   display: flex;
   align-items: center;
+  gap: 10px;
   height: 36px;
   padding-left: 20px;
   padding-right: 20px;
+}
+.heading__listItemArrow {
+  width: 6px;
+  height: 6px;
+  transform: rotate(90deg);
 }
 .heading__listItemText {
   font-family: 'Montserrat-Regular';
@@ -80,19 +77,18 @@ const { title, headingMainList } = defineProps(['title', 'headingMainList']);
   }
   .heading__title {
     font-size: 51px;
-    /* max-width: 480px; */
   }
 }
 
 @media (max-width: 767px) {
-  /* .heading {
-    padding-top: 250px;
-  } */
   .heading__title {
     font-size: 40px;
     max-width: 380px;
     padding-left: 10px;
     padding-right: 10px;
+  }
+  .heading__list {
+    padding-left: 10px;
   }
   .heading__listItem {
     height: 27px;
@@ -105,18 +101,11 @@ const { title, headingMainList } = defineProps(['title', 'headingMainList']);
 }
 
 @media (max-width: 479px) {
-  /* .heading {
-    max-width: 330px;
-  } */
-  .heading__title {
-    font-size: 32px;
-    max-width: 330px;
+  .heading__list {
+    margin-top: 10px;
   }
   .heading__listItem {
     height: 24px;
-  }
-  .heading__listItemText {
-    font-size: 14px;
   }
 }
 </style>
--- components/page/AppMainImage.vue
+++ components/page/AppMainImage.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="mainImage">
-    <!-- Изображение для мобилки от 480 до 320 -->
-    <img
-      v-if="isScreenSmall"
-      src="/images/img-01-desktop.webp"
-      alt="Подложка"
-      class="mainImage__imageSmall"
-    />
+    <!-- Изображение для мобилки -->
 
-    <!-- Изображение для мобилки от 768 до 480 -->
     <img
-      v-if="isScreenMedium && !isScreenSmall"
+      v-if="isScreenMedium"
       src="/images/img-01-mobile.webp"
       alt="Подложка"
       class="mainImage__imageMobile"
@@ -28,10 +21,8 @@
 
 <script setup>
 import { useResizeMedium } from '@/use/useResizeMedium';
-import { useResizeSmall } from '@/use/useResizeSmall';
 
 const { isScreenMedium } = useResizeMedium();
-const { isScreenSmall } = useResizeSmall();
 </script>
 
 <style scoped>
@@ -40,25 +31,26 @@ const { isScreenSmall } = useResizeSmall();
   top: 0;
   left: 0;
   width: 100%;
+  height: 800px;
+  border: 2px solid blue;
 }
 .mainImage__imageDesktop {
   width: 100%;
-  height: 800px;
+  height: 100%;
   object-fit: cover;
   object-position: right;
 }
 
 .mainImage__imageMobile {
   width: 100%;
-  height: 700px;
+  height: 100%;
   object-fit: cover;
   object-position: center;
 }
-.mainImage__imageSmall {
-  width: 100%;
-  height: 700px;
-  object-fit: cover;
-  object-position: top 0 right -250px;
-  /* object-position: center; */
+
+@media (max-width: 767px) {
+  .mainImage {
+    height: 700px;
+  }
 }
 </style>
