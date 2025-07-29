6b560459bd4201df857857e57b8d8480c9cb7355
Alexander Morugin <nobilis@bk.ru>
2025-07-29 17:51:24 +0300

    добавил анимацию Бенефиту

--- components/page/AppBenefit.vue
+++ components/page/AppBenefit.vue
@@ -23,16 +23,10 @@
         </div>
       </div>
     </div>
-
-    <!-- Карусель с меняющимися картинками. В десктопе как бекграунд под текстом. В мобильной версии, отдельным блоком -->
-    <!-- <div class="benefit__embla">
-      <EmblaAppFadeCarousel :data="benefitPhoto" />
-    </div> -->
   </section>
 </template>
 
 <script setup>
-import { benefitPhoto } from '@/mock/benefit-photo';
 import { benefit } from '@/mock/benefit';
 </script>
 
@@ -41,28 +35,15 @@ import { benefit } from '@/mock/benefit';
   display: flex;
   flex-direction: column;
   gap: 20px;
-  /* background-image: url('/images/img-benefit-01.webp'); */
-  /* position: relative;
-  height: 900px; */
-
-  /* background: darkgoldenrod; */
-  /* border: 2px solid red; */
 }
-/* .benefit__embla {
-  position: absolute;
-  top: 100px;
-  left: 0;
-} */
 .benefit__background {
   width: 100%;
-  /* height: 800px; */
   background-image: url('/images/img-benefit-01.webp');
   background-repeat: no-repeat;
   background-position: top;
   background-size: cover;
 }
 .benefit__container {
-  /* position: relative; */
   width: 100%;
   max-width: 1440px;
   margin: 0 auto;
@@ -70,9 +51,6 @@ import { benefit } from '@/mock/benefit';
   padding-bottom: 40px;
   padding-left: 20px;
   padding-right: 20px;
-  /* z-index: 20; */
-
-  /* border: 1px solid red; */
 }
 .benefit__list {
   display: flex;
@@ -84,8 +62,6 @@ import { benefit } from '@/mock/benefit';
   gap: 20px;
   width: 100%;
   max-width: 530px;
-
-  /* border: 1px solid red; */
 }
 .benefit__image {
   width: 65px;
@@ -99,8 +75,27 @@ import { benefit } from '@/mock/benefit';
 .benefit__title {
   font-family: 'Montserrat-Regular';
   font-size: 20px;
-  color: var(--blue-primary);
   text-transform: uppercase;
+  background: linear-gradient(
+    to right,
+    var(--white-primary) 20%,
+    var(--blue-primary) 30%,
+    var(--purple-primary) 50%,
+    var(--white-primary) 70%,
+    var(--blue-primary) 80%
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-fill-color: transparent;
+  background-size: 500% auto;
+  animation: textShine 5s ease-in-out infinite alternate;
+}
+.benefit__listItem:nth-child(2) .benefit__title {
+  animation: textShine 4s ease-in-out infinite alternate;
+}
+.benefit__listItem:nth-child(3) .benefit__title {
+  animation: textShine 6s ease-in-out infinite alternate;
 }
 .benefit__text {
   color: var(--white-primary);
@@ -115,62 +110,21 @@ import { benefit } from '@/mock/benefit';
     background-blend-mode: multiply;
     background-position: top 0 right -350px;
     background-color: rgba(0, 0, 0, 0.3);
-    
   }
   .benefit__listItem {
     flex-direction: column;
   }
   .benefit__button {
     padding-left: 0;
-    /* padding-right: 16px; */
+  }
+}
+
+@keyframes textShine {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
   }
 }
 </style>
-
-<!-- <template>
-  <section class="benefit">
-    Картинка в виде бекграунда
-    <img
-      src="/images/img-benefit.webp"
-      alt="Выгоды сотрудничества"
-      class="benefit__image"
-    />
-
-    <div class="benefit__container"></div>
-  </section>
-</template>
-
-<style scoped>
-.benefit {
-  position: relative;
-  width: 100%;
-  height: 750px;
-}
-.benefit__image {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 750px;
-  object-fit: cover;
-  object-position: top;
-}
-.benefit__container {
-  position: relative;
-  width: 100%;
-  max-width: 1440px;
-  height: 100%;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-  z-index: 20;
-  border: 2px solid white;
-}
-
-@media (max-width: 767px) {
-  .benefit__container {
-    padding-left: 10px;
-    padding-right: 10px;
-  }
-}
-</style> -->
